ae6ab9c84e487a61745ee1277fe6b984b1a818e1
Rickard Lindroth <rickard.lindroth@gmail.com>
2016-01-28 11:48:37 +0100

    Finished implementation

--- ocr.py
+++ ocr.py
@@ -5,12 +5,16 @@ import math
 
 invoice_number = sys.argv[1]
 
+#The caclulated ocr number contains three parts. The invoice number, lenght digit and checksum digit.
 def ocr(invoice_number):
-	return invoice_number + calculate_checksum(invoice_number)
+	partial_ocr = invoice_number + length_number(invoice_number)
+	return partial_ocr + calculate_checksum(partial_ocr)
 
 def calculate_checksum(invoice_number):
 	checksum = 0
-	for idx, x in enumerate(invoice_number):
+	#The checksum is calculated in reverse, right to left.
+	for idx, x in enumerate(reversed(invoice_number)):
+		#each x is multiplied by a weigth that is 1 or 2 starting on 2
 		weigth = (idx + 1)%2 + 1
 		result = int(x) * weigth
 		if result > 9:
@@ -22,5 +26,9 @@ def calculate_checksum(invoice_number):
 def roundup(x):
     return int(math.ceil(x / 10.0)) * 10
 
-print 'invoice_number', invoice_number
-print 'ocr', ocr(invoice_number)
+# Length is represented by only one digit, 3, 13, 23 are all represented by 3. 5, 15, 25 by 5 etc.
+# The length is the invoice number including the length digit and checksum digit, therefor the +2
+def length_number(invoice_number):
+	return str((len(invoice_number)+2) % 10)
+
+print 'ocr:', ocr(invoice_number)
